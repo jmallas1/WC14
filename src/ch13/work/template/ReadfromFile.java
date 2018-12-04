74471b83ecebf9519407297cf92770551cc48080
Jared R. Mallas <jrmallas@icloud.com>
2018-12-04 07:10:06 -0600

    todos and instructions for facade

--- src/ch13/work/template/ReadfromFile.java
+++ src/ch13/work/template/ReadfromFile.java
@@ -1,18 +1,34 @@
 package ch13.work.template;
 
-public class ReadfromFile extends BookReader{
+import ch13.work.io.*;
+
+public class ReadfromFile extends BookReader
+{
+    FileInput fi;
     @Override
-    void initialize() {
+    void initialize()
+    {
         System.out.println("Open an input file");
+        fi = new FileInput("./src/ch13/work/template/prideprejudice");
     }
 
     @Override
-    void startReading() {
+    void startReading()
+    {
+        String someString;
         System.out.println("Loop writing out each record");
+        someString = fi.readLine();
+        while (someString != null)
+        {
+            System.out.println(someString + "\n");
+            someString = fi.readLine();
+        }
     }
 
     @Override
-    void stopReading() {
+    void stopReading()
+    {
         System.out.println("Close input file");
+        fi.fileClose();
     }
 }
